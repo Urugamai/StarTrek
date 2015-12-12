832c682a1c25d6d98c99d829678e42c45837f518
urugamai <Mark.Watson@SVR4.com.au>
2015-12-12 17:09:08 +1100

    Version 0.2.3 update

--- src/Sector.java
+++ src/Sector.java
@@ -4,20 +4,21 @@ import java.util.ArrayList;
  * Created by Mark on 8/12/2015.
  */
 public class Sector {
-	private Game				game;						/** The game in which this entity exists */
-	private int 				enemyCount 			= 0;
-	private int 				starbaseCount 		= 0;
-	private float 				starGravity 		= 0;
-	private int					planetCount			= 0;
+	private Game game;
+	/**
+	 * The game in which this entity exists
+	 */
+	private int enemyCount = 0;
+	private int starbaseCount = 0;
+	private float starGravity = 0;
+	private int planetCount = 0;
 
-	private PlayerShipEntity	ship;
-	private TorpedoEntity[]		shots;
-	private int					shotIndex;
+	private PlayerShipEntity ship;
+	private TorpedoEntity[] shots;
+	private int shotIndex;
 
-	private boolean				LRS					= false;
-
-	private ArrayList<Entity> 	entities			= new ArrayList<Entity>();
-	private ArrayList<Entity>	removeList			= new ArrayList<Entity>();
+	private ArrayList<Entity> entities = new ArrayList<Entity>();
+	private ArrayList<Entity> removeList = new ArrayList<Entity>();
 
 	public Sector(Game game) {
 		this.game = game;
@@ -25,11 +26,10 @@ public class Sector {
 		newEnemyCount();
 
 		starbaseCount = Math.random() < Constants.starbaseProbability ? 1 : 0;
-		LRS = (starbaseCount > 0);
 
-		starGravity = (int)(Math.random()*(Constants.maxGravity+1));
+		starGravity = (int) (Math.random() * (Constants.maxGravity + 1));
 
-		planetCount = (int)(Math.random()*(Constants.maxPlanets + 1) );
+		planetCount = (int) (Math.random() * (Constants.maxPlanets + 1));
 
 		// setup n shots
 		shots = new TorpedoEntity[15];
@@ -38,7 +38,9 @@ public class Sector {
 		}
 	}
 
-	public Sprite getSprite(String ref) { return game.getSprite(ref); }
+	public Sprite getSprite(String ref) {
+		return game.getSprite(ref);
+	}
 
 	public int getEnemyCount() {
 		return enemyCount;
@@ -57,7 +59,9 @@ public class Sector {
 		return starGravity;
 	}
 
-	public int getPlanetCount() { return planetCount; }
+	public int getPlanetCount() {
+		return planetCount;
+	}
 
 	/**
 	 * Initialise the starting state of the entities (ship and aliens). Each
@@ -67,46 +71,53 @@ public class Sector {
 		Entity newEntity = null;
 
 		// Whack the star into the middle of the sector
-		newEntity = new StarEntity(game, Constants.FILE_IMG_STAR, width/2, height/2);
+		newEntity = new StarEntity(game, Constants.FILE_IMG_STAR, width / 2, height / 2);
 		entities.add(newEntity);
 
 		// whack in a starbase if needed
 		newEntity = null;
 		for (int i = 0; i < starbaseCount; i++) {
 			do {
-				if (newEntity != null) {newEntity = null; } // dispose of dud selection
-				newEntity = new FriendlyEntity(game, Constants.FILE_IMG_STARBASE, (int)(Math.random()*(width-50)), (int)(Math.random()*(height - 50)));
-			} while ( checkEntityForOverlap(newEntity));
+				if (newEntity != null) {
+					newEntity = null;
+				} // dispose of dud selection
+				newEntity = new FriendlyEntity(game, Constants.FILE_IMG_STARBASE, (int) (Math.random() * (width - 50)), (int) (Math.random() * (height - 50)));
+			} while (checkEntityForOverlap(newEntity));
 			entities.add(newEntity); // this ones a keeper
 		}
 
 		// Whack in the player ship
 		newEntity = null;
 		do {
-			if (newEntity != null) {newEntity = null; } // dispose of dud selection
-			newEntity = new PlayerShipEntity(game, Constants.FILE_IMG_ENTERPRISE, (int)(Math.random()*(width-50)), (int)(Math.random()*(height - 50)));
-		} while ( checkEntityForOverlap(newEntity));
+			if (newEntity != null) {
+				newEntity = null;
+			} // dispose of dud selection
+			newEntity = new PlayerShipEntity(game, Constants.FILE_IMG_ENTERPRISE, (int) (Math.random() * (width - 50)), (int) (Math.random() * (height - 50)));
+		} while (checkEntityForOverlap(newEntity));
 		entities.add(newEntity);
-		ship = (PlayerShipEntity)newEntity;
+		ship = (PlayerShipEntity) newEntity;
 
 		// Whack in the necessary number of enemy units
 		newEntity = null;
 		for (int i = 0; i < enemyCount; i++) {
 			do {
-				if (newEntity != null) {newEntity = null; } // dispose of dud selection
-				newEntity = new EnemyShipEntity(game, Constants.FILE_IMG_ROMULAN, (int)(Math.random()*(width-50)), (int)(Math.random()*(height - 50)));
-			} while ( checkEntityForOverlap(newEntity));
+				if (newEntity != null) {
+					newEntity = null;
+				} // dispose of dud selection
+				newEntity = new EnemyShipEntity(game, Constants.FILE_IMG_ROMULAN, (int) (Math.random() * (width - 50)), (int) (Math.random() * (height - 50)));
+			} while (checkEntityForOverlap(newEntity));
 			entities.add(newEntity); // this ones a keeper
 		}
 	}
 
 	private boolean checkEntityForOverlap(Entity me) {
-		for ( Entity entity : entities) {
+		for (Entity entity : entities) {
 			if (me == entity) continue;
 			if (me.collidesWith(entity)) return true;
 		}
 		return false;
 	}
+
 	/**
 	 * Remove an entity from the game. The entity removed will
 	 * no longer move or be drawn.
@@ -138,8 +149,8 @@ public class Sector {
 	public void notifyAlienKilled() {
 		// if there are still some aliens left then they all need to get faster, so
 		// speed up all the existing aliens
-		for ( Entity entity : entities ) {
-			if ( entity instanceof EnemyShipEntity ) {
+		for (Entity entity : entities) {
+			if (entity instanceof EnemyShipEntity) {
 				// speed up by 2%
 				entity.setHorizontalMovement(entity.getHorizontalMovement() * 1.04f);
 			}
@@ -207,14 +218,14 @@ public class Sector {
 
 	public void move(long msElapsed) {
 		// cycle round asking each entity to move itself
-		for ( Entity entity : entities ) {
+		for (Entity entity : entities) {
 			entity.move(msElapsed);
 		}
 	}
 
 	public void draw() {
 		// cycle round drawing all the entities we have in the game
-		for ( Entity entity : entities ) {
+		for (Entity entity : entities) {
 			entity.draw();
 		}
 	}
@@ -224,17 +235,9 @@ public class Sector {
 
 		// cycle round every entity requesting that
 		// their personal logic should be considered.
-		for ( Entity entity : entities ) {
+		for (Entity entity : entities) {
 			entity.doLogic();
 		}
 	}
-
-	public boolean getSectorLRS() {
-		return true; //TODO restore to LRS once testing completed
-	}
-
-	public void setLRS(boolean lrs) {
-		LRS = lrs;
-		if ( starbaseCount > 0) LRS = true;
-	}
 }
+
